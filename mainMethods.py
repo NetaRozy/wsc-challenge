9cd553ae10ce611470f24d5aff3d88878cab7e0d
Neta Rozy <neta.rozy@gmail.com>
2018-04-27 06:12:30 +0300

    merging helper methods

--- mainMethods.py
+++ mainMethods.py
@@ -7,12 +7,12 @@ def get_game_highlights(home, visitor):
     home_id = wscHelperMethods.get_team_id(home)
     visitor_id = wscHelperMethods.get_team_id(visitor)
 
-    game_id = wscHelperMethods.get_game(home_id, visitor_id)
-    video_id = wscHelperMethods.create_highlights(game_id)
+    game_id = wscHelperMethods.get_game_id(home_id, visitor_id)
+    video_id = wscHelperMethods.create_game_highlights(game_id)
 
     time.sleep(60)
 
-    return video_id
+    return wscHelperMethods.get_video_url(video_id)
 
 def get_game_highlights_by_event(home, visitor, event):
 
@@ -35,3 +35,12 @@ def get_my_video(text):
     vid = wscHelperMethods.get_my_video(text)
     return vid
 
+# def get_vid_url(vid_id):
+#     return wscHelperMethods.get_video_url(vid_id)
+
+# print (get_my_video('spain top plays'))
+# print(get_game_highlights('spain','russia'))
+# print (get_my_video('slovenia serbia'))
+# print (get_my_video('spainsloveniaDunks'))
+# print(get_my_video('spainsloveniaDunks')['videoUrl'])
+# print(get_my_video('Slovenia vs. Serbia'))
